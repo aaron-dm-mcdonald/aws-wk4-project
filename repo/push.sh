b9607298dc3338899ce94aa1d5ed252d22a0e24c
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-10-18 17:05:14 -0700

    setup repo

---
+++ repo/push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# git init
+git add .
+git commit -m "setup repo"
+# git branch -M main
+# git remote add origin https://github.com/aaron-dm-mcdonald/aws-wk4-project.git
+git push #-u origin main
