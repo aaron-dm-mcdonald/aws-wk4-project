f5064f45045e65cf7828903c982f427ff1e6d07e
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-10-18 18:07:04 -0700

    permit ec2 s3 access

--- 7-iam.tf
+++ 7-iam.tf
@@ -1,8 +1,3 @@
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "EC2_S3_Access_Profile"
-  role = aws_iam_role.ec2_role.name
-}
-
 resource "aws_iam_role" "ec2_role" {
   name = "EC2_S3_Access_Role"
 
@@ -21,26 +16,12 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-resource "aws_iam_policy" "s3_policy" {
-  name   = "S3_Access_Policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:ListBucket",
-          "s3:GetObject"
-        ]
-        Resource = [
-          "${aws_s3_bucket.bucket.arn}/*"
-        ]
-      },
-    ]
-  })
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "EC2_S3_Access_Profile"
+  role = aws_iam_role.ec2_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "attach_policy" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.s3_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"  # Attach the AWS managed policy
 }
