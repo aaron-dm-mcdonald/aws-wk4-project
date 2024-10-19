221aecc1d0a6b4898b5f57ce9ec6d058f70b23f6
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-10-18 17:06:46 -0700

    gitignore update

--- repo/push.sh
+++ repo/push.sh
@@ -2,7 +2,7 @@
 
 # git init
 git add .
-git commit -m "setup repo"
+git commit -m "gitignore update"
 # git branch -M main
 # git remote add origin https://github.com/aaron-dm-mcdonald/aws-wk4-project.git
 git push #-u origin main
